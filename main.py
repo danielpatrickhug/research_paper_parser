39306de31ce9c817fc2cf26c6afb312a1cdc0c1a
Daniel Hug <danielpatrickhug@gmail.com>
2021-09-30 22:39:55 -0400

    remove unused arg

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from parser import Parser
 def main():
     arg_parser = ArgumentParser()
     arg_parser.add_argument('-i', '--input', help='Input file path', required=True)
-    #arg_parser.add_argument('-m', '--model_name', help='Model name', required=True)
 
     args = arg_parser.parse_args()
     parser = Parser(args.input)
